d18e1f28a94ad120fa07e33a5e231346f9420d67
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2024-08-24 22:55:01 +0200

    Rework CLI

--- resmetric/cli.py
+++ resmetric/cli.py
@@ -32,45 +32,52 @@ def main():
     # Required argument
     parser.add_argument('json_file', type=str, help='Path to the JSON file containing the data.')
 
+    preprocessing_group = parser.add_argument_group('Preprocessing Options')
+    preprocessing_group.add_argument('--smooth_criminal', action='store_true',
+                                     help='Smooth the series with a threshold-based value update filter (Hee-Hee! Ow!).')
+
     # Group for basic trace options
-    basic_group = parser.add_argument_group('Basic Trace Options')
+    basic_group = parser.add_argument_group('Core Resilience-Related Trace Options')
     basic_group.add_argument('--auc', action='store_true', help='Include AUC-related traces.')
     basic_group.add_argument('--count', action='store_true',
                              help='Include traces that count dips below the threshold.')
     basic_group.add_argument('--time', action='store_true',
                              help='Include traces that accumulate time below the threshold.')
     basic_group.add_argument('--threshold', type=float, default=80,
-                             help='Threshold for count and time traces (default: 80).')
-    basic_group.add_argument('--drawdowns_traces', action='store_true',
-                             help='Include the values of local drawdowns as traces.')
-    basic_group.add_argument('--drawdowns_shapes', action='store_true',
-                             help='Include the shapes of local drawdowns.')
-    basic_group.add_argument('--smooth_criminal', action='store_true',
-                             help='Include smoothed series (Hee-Hee! Ow!).')
-    basic_group.add_argument('--dips', action='store_true', help='Include detected dips.')
+                             help='Threshold for count and time traces in percent (default: 80).')
     basic_group.add_argument('--max_dips', action='store_true',
                              help='Include maximal dips, maximal draw-downs, and recoveries')
     basic_group.add_argument('--bars', action='store_true', help='Include bars for MDD and recovery.')
-    basic_group.add_argument('--deriv', action='store_true', help='Include derivatives traces.')
-    basic_group.add_argument('--lg', action='store_true', help='Include linear regression traces.')
 
-    # Group for Bayesian Optimization arguments
-    bayesian_group = parser.add_argument_group('Bayesian Optimization (Only for --lg)')
-    bayesian_group.add_argument('--penalty_factor', type=float, default=0.05,
-                                help='Penalty factor for the number of segments in Bayesian optimization (default: '
-                                     '0.05).')
-    bayesian_group.add_argument('--dimensions', type=int, default=10,
-                                help='Give the maximal number of segments for lin reg')
+    anti_fragility_group = parser.add_argument_group('Resilience-Related Metrics over Time Options ("Anti-Fragility")')
+    anti_fragility_group.add_argument('--place-holder', action='store_true', help='place holder')
 
-    # Group for AUC-related arguments
-    auc_group = parser.add_argument_group('AUC-related Options (Only for --auc)')
-    auc_group.add_argument('--weighted_auc_half_life', type=float, default=2,
-                           help='Half-life for weighted AUC calculation (default: 2).')
+    experimental_group = parser.add_argument_group('Experimental Options [To be expanded in the future]')
+    experimental_group.add_argument('--deriv', action='store_true', help='Include derivatives traces.')
+    experimental_group.add_argument('--lg', action='store_true', help='Include linear regression traces.')
 
-    # Group for Smoothing function arguments
-    smooth_group = parser.add_argument_group('Smoothing Function Options (Only for --smooth-criminal)')
-    smooth_group.add_argument('--smoother_threshold', type=float, default=2,
-                              help='Threshold for the smoothing function (default: 2).')
+    fine_group = parser.add_argument_group('[Advanced] Fine-Grained Analysis Trace Options')
+    fine_group.add_argument('--dips', action='store_true', help='Include all detected dips.')
+    fine_group.add_argument('--drawdowns_traces', action='store_true',
+                            help='Include the values of local drawdowns as traces.')
+    fine_group.add_argument('--drawdowns_shapes', action='store_true',
+                            help='Include the shapes of local drawdowns.')
+
+    # Group for Advanced Settings
+    settings_group = parser.add_argument_group('[Advanced] Settings')
+    # Bayesian Optimization arguments
+    settings_group.add_argument('--penalty_factor', type=float, default=0.05,
+                                help='[--lg only] Penalty factor for the number of segments in Bayesian optimization '
+                                     '(default: 0.05).')
+    settings_group.add_argument('--dimensions', type=int, default=10,
+                                help='[--lg only] Give the maximal number of segments for lin reg')
+    # AUC-related arguments
+    settings_group.add_argument('--weighted_auc_half_life', type=float, default=2,
+                                help='[--auc only] Half-life for weighted AUC calculation (default: 2).')
+    # Smoothing function arguments
+    settings_group.add_argument('--smoother_threshold', type=float, default=2,
+                                help='[--smooth-criminal only] Threshold for the smoothing function in percent ('
+                                     'default: 2).')
 
     # Group for display or save options
     display_group = parser.add_argument_group('Display or Save Options')
