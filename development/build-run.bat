8f911b3b32678853dc971122f366bb1c8fa64fed
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2024-09-27 20:44:10 +0200

    Add more possible test cases to build-run.bat

--- development/build-run.bat
+++ development/build-run.bat
@@ -2,4 +2,6 @@
 @REM Adapt the last line for the case you want to test during development.
 pip uninstall -y resmetric
 pip install .
-resmetric-cli --max_dips --bars --max-dip-auc --calc-res-over-time ".\example\fig.json"
+@REM resmetric-cli --max_dips --bars --max-dip-auc --calc-res-over-time ".\example\fig.json"
+@REM resmetric-cli --gr --calc-res-over-time ".\example\fig.json"
+resmetric-cli --lin-reg ".\example\fig.json"
