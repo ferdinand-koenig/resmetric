8e195070c0d25b14fa5fe7bc19983b866b1df556
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2024-10-14 14:26:41 +0200

    Change setup.py to reflect rc1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='resmetric',
-    version='1.0.0-beta',
+    version='1.0.0-rc.1',
     description='A Python module for enhancing Plotly figures with resilience-related metrics.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
